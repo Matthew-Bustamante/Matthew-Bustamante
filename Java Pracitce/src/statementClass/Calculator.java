30d3087f5c727790ea7ed4584daedece2fd1bb19
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-02-01 23:39:07 -0700

    Added documentation

--- Java Pracitce/src/statementClass/Calculator.java
+++ Java Pracitce/src/statementClass/Calculator.java
@@ -1,27 +1,33 @@
 package statementClass;
 
 public class Calculator {
+	//variables to keep track of the numbers
 	private int number1;
 	private int number2;
 	
 	public Calculator(int numberOne, int numberTwo) {
+		//calculator constructor
 		number1 = numberOne;
 		number2 = numberTwo;
 	}
 	
 	public int add() {
+		//adds the two numbers
 		return number1 + number2;
 	}
 	
 	public int subtract() {
+		//subtracts the two numbers
 		return number1 - number2;
 	}
 	
 	public int multiply() {
+		//multiplies the two numbers
 		return number1 * number2;
 		}
 	
 	public int divide() {
+		//divides the two numbers
 		return number1 / number2;
 		}
 	}
