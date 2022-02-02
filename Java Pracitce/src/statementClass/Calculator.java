af4155ff536ae56634d95260168de6cda862e8fc
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-02-01 23:16:38 -0700

    Added a Calculator Class

---
+++ Java Pracitce/src/statementClass/Calculator.java
@@ -0,0 +1,19 @@
+package statementClass;
+
+public class Calculator {
+	private int number1;
+	private int number2;
+	
+	public Calculator(int numberOne, int numberTwo) {
+		number1 = numberOne;
+		number2 = numberTwo;
+	}
+	
+	public int add() {
+		return number1 + number2;
+	}
+	
+	public int subtract() {
+		return number1 - number2;
+	}
+}
